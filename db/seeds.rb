e84bc8e74d970b896cc16c2c4d0490e57ba7ecfc
Nini Zhu <pn.zhu76@gmail.com>
2019-03-27 15:18:29 +0800

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.delete_all if Rails.env.development?
+
+4.times do
+  Flat.create!(name: Faker::University.name, address: Faker::Address.full_address, description: Faker::Movies::PrincessBride.quote, price_per_night: rand(64..2000), number_of_guests: rand(1..8))
+end
+
+puts "you've created #{Flat.count} flats"
